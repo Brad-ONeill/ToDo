68cf17060513302f8f8c3fdf56cc28af62cb87d7
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-09-16 15:05:40 +0100

    Refactor of entire code with styling

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,68 @@
 <template>
-  <div>
-    <input
-      v-model="currentTodo"
-      @keydown.enter="addTodo()"
-      placeholder="Add a todo"
-    />
-    <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id">
-        <div>
-          <input
-            type="checkbox"
-            v-on:click="done(todo)"
-            v-bind:checked="todo.done"
-          />
-          <label @dblclick="todo.edit = true">
-            <del v-if="todo.done"> {{ todo.label }} </del>
-            <span v-else>{{ todo.label }}</span>
-          </label>
-          <button v-on:click="removeTodo(todo.id)">X</button>
-        </div>
-        <input
-          v-show="todo.edit == true"
-          v-model="todo.label"
-          v-on:blur="
-            todo.edit = false;
-            $emit('update');
-          "
-          @keyup.enter="
-            todo.edit = false;
-            $emit('update');
-          "
-        />
-      </li>
-    </ul>
+  <div id="container">
+    <div class="md-layout">
+      <div class="md-layout-item">
+        <md-card>
+          <md-card-content>
+            <md-card-header>Your <b>To Do</b> list</md-card-header>
+            <div>
+              <md-card-content>
+                <input
+                  class="todoInput"
+                  v-model="currentTodo"
+                  @keydown.enter="addTodo()"
+                  placeholder="Add a todo"
+                />
+
+                <ul class="todos">
+                  <li v-for="todo in todos" :key="todo.id">
+                    <div>
+                      <span>
+                        <input
+                          class="md-primary"
+                          type="checkbox"
+                          v-on:click="done(todo)"
+                          v-bind:checked="todo.done"
+                        />
+                      </span>
+
+                      <span id="todoItem">
+                        <label @dblclick="todo.edit = true">
+                          <del v-if="todo.done"> {{ todo.label }} </del>
+                          <span v-else>{{ todo.label }}</span>
+                        </label>
+                      </span>
+
+                      <span class="xButton">
+                        <md-button
+                          class="md-accent"
+                          v-on:click="removeTodo(todo.id)"
+                          >&times;</md-button
+                        >
+                      </span>
+                    </div>
+
+                    <input
+                      id="todoEdit"
+                      v-show="todo.edit == true"
+                      v-model="todo.label"
+                      v-on:blur="
+                        todo.edit = false;
+                        $emit('update');
+                      "
+                      @keyup.enter="
+                        todo.edit = false;
+                        $emit('update');
+                      "
+                    />
+                  </li>
+                </ul>
+              </md-card-content>
+            </div>
+          </md-card-content>
+        </md-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -69,4 +99,90 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: top;
+  padding: 50px;
+}
+
+#container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.md-layout {
+  min-width: 60%;
+}
+
+.md-card-header {
+  font-size: 22px;
+  text-align: center;
+}
+
+#todoEdit {
+  font-family: "Courier New", Courier, monospace;
+  margin: 0 0 5px 40px;
+  border: 1px dotted gray;
+  border-radius: 4px;
+  padding: 4px;
+  color: darkred;
+  font-size: 12px;
+}
+
+textarea:focus,
+input:focus {
+  outline: none;
+}
+
+.todoInput {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 100%;
+  border-top: 0;
+  border-right: 0;
+  border-bottom: 1px dotted;
+  border-left: 0;
+  padding: 0 0 5px 0;
+  margin-bottom: 20px;
+}
+
+ul {
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+#todoItem {
+  padding: 0 15px;
+  vertical-align: top;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+button.md-button {
+  align-items: flex-start;
+  margin: 0 5px;
+  min-width: 20px;
+  width: 20px;
+  height: 20px;
+}
+
+.md-button {
+  min-width: 20px;
+  margin: 0;
+  padding: 0;
+}
+
+.xButton {
+  float: right;
+}
+</style>
