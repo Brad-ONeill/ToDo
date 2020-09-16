0897f3fe6ab5f304740b5cdf799525c580bbfe73
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-09-16 15:05:18 +0100

    Import of card for styling

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+
+import { MdCard } from "vue-material/dist/components";
+
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 
 Vue.use(VueMaterial);
+Vue.use(MdCard);
 
 Vue.config.productionTip = false;
 
